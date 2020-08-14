03dbb254d94ccda59e108b92748a53c598959d30
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-08-14 15:44:54 -0300

    04-parte 1

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,16 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForeclastController } from './controllers/forecast';
 import { Application } from 'express';
+import * as database from '@src/database'
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.databaseSetup();
   }
   private setupExpress(): void {
     this.app.use(bodyParser.json());
@@ -18,6 +20,12 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForeclastController();
     this.addControllers([forecastController]);
+  }
+  private async databaseSetup(): Promise<void>{
+    await database.connect();
+  }
+  public async close(): Promise<void>{
+    await database.close();
   }
   public getApp(): Application {
     return this.app;
