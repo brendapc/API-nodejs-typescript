038235f6449915b9f3a3c9b1d90bfc6a312c5d5d
Brenda Pereira <33674561+brendapc@users.noreply.github.com>
2020-08-28 14:12:23 -0300

    integrando rating e forecast

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -2,6 +2,7 @@ import { StormGlass, ForecastPoint } from '@src/clients/stormGlass'
 import { InternalError } from '@src/util/errors/internal-error';
 import { Beach } from '@src/models/beach';
 import logger from '@src/logger';
+import { Rating } from './rating';
 
 export interface BeachForecast extends Omit<Beach , 'user'>, ForecastPoint{}
 
@@ -20,15 +21,18 @@ export class ForecastProcessingInternalError extends InternalError{
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
 export class Forecast {
-    constructor(protected stormGlass = new StormGlass()){ }
+    constructor(protected stormGlass = new StormGlass(), protected RatingService: typeof Rating = Rating){ 
+
+    }
 
     public async processForecastForBeaches(beaches: Beach[]): Promise<TimeForecast[]>{
         const pointsWithCorrectSources: BeachForecast[] = [];
         logger.info(`Preparing the forecast for ${beaches.length} beaches`)
         try{
             for(const beach of beaches){
+                const rating = new this.RatingService(beach)
                 const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
-                const enrichedBeachData = this.enrichedBeachData(points, beach)
+                const enrichedBeachData = this.enrichedBeachData(points, beach, rating)
                 pointsWithCorrectSources.push(...enrichedBeachData)
             }
             return this.mapForecastByTime(pointsWithCorrectSources)
@@ -39,18 +43,19 @@ export class Forecast {
     }
     private enrichedBeachData(
         points: ForecastPoint[], 
-        beach: Beach
-        ): BeachForecast[]{
-        return points.map((e)=>({
+        beach: Beach,
+        rating: Rating
+    ): BeachForecast[]{
+        return points.map((point)=>({
             ...{},
             ...{
                 lat: beach.lat,
                 lng: beach.lng,
                 name: beach.name,
                 position: beach.position,
-                rating: 1 /* need to be implemented */
+                rating: rating.getRateForPoint(point)
             },
-            ...e,
+            ...point,
         }));
     }
 
